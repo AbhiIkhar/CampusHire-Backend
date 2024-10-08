ee79c8bab7f23453ed03c27ac7bd25b9e5eef55e
Abhishek <ikharabhishek177@gmail.com>
2024-10-08 18:23:42 +0530

    CORS fixing

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.options("*", cors(corsOptions));
+
 app.listen(PORT, () => {
   connectDB();
 });
